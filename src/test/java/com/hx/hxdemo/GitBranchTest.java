1bdbf947c30728f69f6029828cad41de6af31db0
huaxiao <huaxiao@jd.com>
2019-08-22 10:44:19 +0800

    test3

--- src/test/java/com/hx/hxdemo/GitBranchTest.java
+++ src/test/java/com/hx/hxdemo/GitBranchTest.java
@@ -5,5 +5,6 @@ public class GitBranchTest {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("test3");
     }
 }
