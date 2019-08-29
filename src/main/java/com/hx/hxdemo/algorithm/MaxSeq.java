b9b465583b0df602a2d76ee71d47ac10807154d7
huaxiao <huaxiao@jd.com>
2019-08-29 19:23:04 +0800

    添加注释

--- src/main/java/com/hx/hxdemo/algorithm/MaxSeq.java
+++ src/main/java/com/hx/hxdemo/algorithm/MaxSeq.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 /**
  * 最大子序列和
+ * todo -- 运行结果不对
  */
 public class MaxSeq {
 
@@ -20,20 +21,29 @@ public class MaxSeq {
 //            result.add(Arrays.copyOfRange(args,i,args.length));
 
             for(int j=i;j< args.length;j++){
+                List tempResult = Lists.newArrayList();
                 sum+=args[j];
                 if(sum>max){
                     max=sum;
+                    tempResult.add(j);
 //                    ret = null;
-                    ret = Arrays.copyOfRange(args,i,args.length-1);
+//                    ret = Arrays.copyOfRange(args,i,args.length-1);
                 }
+                result = tempResult;
+//                tempResult = null;
             }
         }
         System.out.println(String.format("max num is: %s",max+""));
+        System.out.println("resultList is :"+String.join(",",result));
+        for (int i = 0; i < result.size(); i++) {
+            ret[i] = Integer.parseInt((String) result.get(i));
+
+        }
         return ret;
     }
 
     public static void main(String[] args) {
-        int[] arr = {-1,1,2,-2};
+        int[] arr = {-1,1,2,-2,6};
         int[] ret = new MaxSeq().getMaxSumSequenceOn2(arr);
         System.out.println(Arrays.toString(ret));
     }
