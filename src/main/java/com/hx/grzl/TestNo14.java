85101c1226096fe5973fcdf4f9ea08cee34aae65
huaxiao <181973244@qq.com>
2020-04-06 22:39:54 +0800

    add python programs.

---
+++ src/main/java/com/hx/grzl/TestNo14.java
@@ -0,0 +1,23 @@
+package com.hx.grzl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 习题14、把字符串中的所有数字去掉。
+ * Created by huaxiao on 2020/4/6.
+ */
+public class TestNo14 {
+
+    public static void main(String[] args) {
+        String s = "a1b2c3b4d5dddddd";
+        List<String> ret = new ArrayList<>();
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if(!"0123456789".contains(String.valueOf(charArray[i]))){
+                ret.add(String.valueOf(charArray[i]));
+            }
+        }
+        System.out.println(String.join(",",ret));
+    }
+}
