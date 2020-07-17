038a6a5e56fbea1a9b69af526bee221ee4b00de9
huaxiao <huaxiao@jd.com>
2020-07-17 10:32:07 +0800

    add practice.

--- src/main/java/com/hx/grzl/HomeWorkNo18.java
+++ src/main/java/com/hx/grzl/HomeWorkNo18.java
@@ -1,7 +1,6 @@
 package com.hx.grzl;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * 课后习题18、实现一个简单的单词本
@@ -13,14 +12,64 @@ import java.util.Scanner;
  **/
 public class HomeWorkNo18 {
     public static void main(String[] args) {
-        System.out.println("please input the five name sep by , : ");
+        String info = "add: add the word and word mean \n" +
+                "find: find the word \n" +
+                "del : delete the word\n" +
+                "all : show all word\n" +
+                "bye :quit the program \n";
+        System.out.println(info);
 
-        Scanner sc = new Scanner(System.in);
-        String str = new String();
-        str = sc.nextLine();
-        System.out.println("names: " + str);
-        String[] arr = str.split(",");
-        Arrays.sort(arr);
-        System.out.println("result="+Arrays.asList(arr));
+        Map wordListDict = new HashMap<>();
+        while (true) {
+            System.out.println("please input the command:");
+            Scanner sc = new Scanner(System.in);
+            String command = new String();
+            command = sc.nextLine();
+//            System.out.println("command: " + command);
+            if (command.equals("add")) {
+                System.out.println("please input the word:");
+                Scanner sc1 = new Scanner(System.in);
+                String word = new String();
+                word = sc1.nextLine();
+                if (wordListDict.containsKey(word)) {
+                    System.out.println("alreay exists");
+                } else {
+                    System.out.println("input the word mean: ");
+                    Scanner sc2 = new Scanner(System.in);
+                    String wordMean = new String();
+                    wordMean = sc2.nextLine();
+                    wordListDict.put(word, wordMean);
+                }
+            } else if (command.equals("find")) {
+                System.out.println("please input the word:");
+                Scanner sc3 = new Scanner(System.in);
+                String word = new String();
+                word = sc3.nextLine();
+                if (wordListDict.containsKey(word)) {
+                    System.out.println(wordListDict.get(word));
+                } else {
+                    System.out.println("not exists");
+                }
+
+            } else if (command.equals("del")) {
+                System.out.println("please input the word:");
+                Scanner sc3 = new Scanner(System.in);
+                String word = new String();
+                word = sc3.nextLine();
+                if (wordListDict.containsKey(word)) {
+                    wordListDict.remove(word);
+                    System.out.println("delete success.");
+                } else {
+                    System.out.println("not exists");
+                }
+            } else if (command.equals("all")) {
+                System.out.println("current words are: \n"+ wordListDict);
+            }else if (command.equals("bye")) {
+                System.out.println("see you~");
+                break;
+            }else {
+                System.out.println("please input the right command.");
+            }
+        }
     }
 }
